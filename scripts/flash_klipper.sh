da080abdbfb860ed9328ab968e742c450025f06b
Jason Ragsdale <jrags@jasrags.net>
2021-11-21 12:12:21 -0600

    Added scripts directory, updated .gitignore

--- scripts/flash_klipper.sh
+++ scripts/flash_klipper.sh
@@ -7,17 +7,16 @@ make clean KCONFIG_CONFIG=config.spider
 make menuconfig KCONFIG_CONFIG=config.spider
 make KCONFIG_CONFIG=config.spider
 read -p "Spider firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-
 ./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
 read -p "Spider firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 # Flash MCU (Linux)
 make clean KCONFIG_CONFIG=config.rpi
 make menuconfig KCONFIG_CONFIG=config.rpi
-
 make KCONFIG_CONFIG=config.rpi
 read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=config.rpi
+read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 # Flash MCU (Expander)
 # make clean KCONFIG_CONFIG=config.rpi
@@ -25,5 +24,6 @@ make flash KCONFIG_CONFIG=config.rpi
 # make KCONFIG_CONFIG=config.expander
 # read -p "Expander firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 # make flash KCONFIG_CONFIG=config.expander FLASH_DEVICE=0483:df11
+# read -p "Expander firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 sudo service klipper start
